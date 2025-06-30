dc8ff288f91d54d2bd453496230dede652170caa
Stephon Brown <stephonb14@gmail.com>
2025-06-30 01:24:45 -0400

    Exports new modal components
    Exports the `WorkoutStatsModal` and `WorkoutEditModal` components
    to make them available for use in other parts of the application.

--- src/modals/index.ts
+++ src/modals/index.ts
@@ -3,3 +3,5 @@ export { ExerciseModal } from './ExerciseModal';
 export { ExerciseTemplateModal } from './ExerciseTemplateModal';
 export { QuickWorkoutModal } from './QuickWorkoutModal';
 export { WorkoutTypeSelectionModal } from './WorkoutTypeSelectionModal';
+export { WorkoutStatsModal } from './WorkoutStatsModal';
+export { WorkoutEditModal } from './WorkoutEditModal';
